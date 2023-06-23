c59fd40f26fdff8d9463d5a7887ba4a3e68d96ac
xerocross <adamfgcross@gmail.com>
2023-06-23 12:35:43 -0400

    Add vite config for worker

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
-import { fileURLToPath, URL } from "node:url"
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite"
-import vue from "@vitejs/plugin-vue"
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import workerPlugin from "worker-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins : [vue()],
+    plugins : [vue(),  workerPlugin()],
     resolve : {
         alias : {
             "@" : fileURLToPath(new URL("./src", import.meta.url))
         }
+    },
+    build : {
+        worker : {
+            entry : "src/get-factor-worker.js",
+            output : "/"
+        }
     }
-})
+});
