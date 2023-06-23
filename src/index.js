dc65cb6b0040823bbd543e4aa3b55debab62c521
xerocross <adamfgcross@gmail.com>
2023-06-23 11:30:43 -0400

    add logic for handling browser has no worker

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import "./assets/main.css";
-
 import { createApp } from "vue";
 import XeroFactor2 from "./components/XeroFactor2.vue";
 
-const myWorker = new Worker("js/get-factor-worker.js");
-
 try {
+    let myWorker;
+    if (typeof Worker === "function") {
+        myWorker = new Worker(new URL("./get-factor-worker.js", import.meta.url), {
+            type : "module"
+        });
+    }
     createApp(XeroFactor2, {
         worker : myWorker
-    })
-        .mount("#xero-factor-2");
+    }).mount("#xero-factor-2");
+    
 }
 catch(e) {
     console.log("mounting error", e);
+    throw e;
 }
