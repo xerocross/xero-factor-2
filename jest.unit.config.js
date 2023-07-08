774b213aaca2c0fb150a4cde8829442bebc4f98f
Adam Cross <adamfgcross@gmail.com>
2023-07-08 16:48:08 -0400

    add jest typescript stuff

--- jest.unit.config.js
+++ jest.unit.config.js
@@ -1,5 +1,19 @@
 /* global module */
 module.exports = {
+    moduleFileExtensions : ["js", "jsx", "ts", "tsx", "json", "vue"],
+    preset : "ts-jest",
+    testEnvironment : "jsdom",
+    transform : {
+        "^.+\\.vue$" : "@vue/vue3-jest",
+        "^.+\\.tsx?$" : "ts-jest"
+    },
+    moduleNameMapper : {
+        "^@/(.*)$" : "<rootDir>/src/$1"
+    },
+    testMatch : ["**/*.spec.(js|jsx|ts|tsx)"],
+    transformIgnorePatterns : ["/node_modules/"],
+    collectCoverageFrom : ["src/**/*.{js,jsx,ts,tsx,vue}", "!src/main.ts"],
+    coverageReporters : ["html", "text-summary"],
     testPathIgnorePatterns : [
         "/tests/integration/"
     ]
